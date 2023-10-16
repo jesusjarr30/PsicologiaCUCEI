8caeaad4e7afc2b81ca6a450a88208b6961cae27
jesusjarr30 <jesus.jarr.30@gmail.com>
2023-10-15 19:56:51 -0600

    add validations to the controllers

--- frontLaravel/app/Http/Controllers/citaController.php
+++ frontLaravel/app/Http/Controllers/citaController.php
@@ -6,6 +6,7 @@ use App\Models\Cliente;
 use App\Models\Comentario;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
 
 
 class citaController extends Controller
@@ -21,42 +22,61 @@ class citaController extends Controller
     }
 
     public function store(Request $request): RedirectResponse
-    {
-        
-        $nombre= $request->input('nombre');
-        $apellidos= $request->input('apellidos');
-        $codigo= $request->input('codigo');
-        $correo= $request->input('correo');
-        $codigo= $request->input('codigo');
-        $edad= $request->input('edad');
-        $telefono= $request->input('telefono');
-        $nacimiento= $request->input('nacimiento');
-        //obtener los campos para generar la tabla de comentarios 
-        $descripcion = $request->input('descripcion');
-        $expectativas = $request->input('expectativas');
-        $horario = $request->input('horario');
+{
+    $nombre = $request->input('nombre');
+    $apellidos = $request->input('apellidos');
+    $codigo = $request->input('codigo');
+    $correo = $request->input('correo');
+    $edad = $request->input('edad');
+    $telefono = $request->input('telefono');
+    $nacimiento = $request->input('nacimiento');
+    // Obtener los campos para generar la tabla de comentarios
+    $descripcion = $request->input('descripcion');
+    $expectativas = $request->input('expectativas');
+    $horario = $request->input('horario');
 
-        $tabla1 = new Cliente();
-        $tabla1->nombre = $nombre;
-        $tabla1->apellidos = $apellidos;
-        $tabla1->codigo = $codigo;
-        $tabla1->correo = $correo;
-        $tabla1->edad = $edad;
-        $tabla1->telefono = $telefono;
-        $tabla1->nacimiento= $nacimiento;
-        $tabla1->save();
+    // Validación
+    $validator = Validator::make($request->all(), [
+        'nombre' => 'required|string|max:255',
+        'apellidos' => 'required|string|max:255',
+        'codigo' => 'required|string|max:10',
+        'correo' => 'required|email',
+        'edad' => 'required|integer|min:16', // Cambia 18 por el valor mínimo requerido
+        'telefono' => 'required|string|max:12',
+        'nacimiento' => 'required|date',
+        'descripcion' => 'required|string',
+        'expectativas' => 'required|string',
+        'horario' => 'required|string',
+    ]);
 
-        $cliente_id = $tabla1->id;
-
-        $tabla2 = new Comentario();
-        $tabla2->cliente_id = $cliente_id;
-        $tabla2->descripcion = $descripcion;
-        $tabla2->expectativas = $expectativas;
-        $tabla2->horario = $horario;
-
-        $tabla2->save();
-        return redirect()->route('home');
+    if ($validator->fails()) {
+        // Si la validación falla, redirige de vuelta con los errores
+        return redirect()->back()
+            ->withErrors($validator);
     }
+
+    $tabla1 = new Cliente();
+    $tabla1->nombre = $nombre;
+    $tabla1->apellidos = $apellidos;
+    $tabla1->codigo = $codigo;
+    $tabla1->correo = $correo;
+    $tabla1->edad = $edad;
+    $tabla1->telefono = $telefono;
+    $tabla1->nacimiento = $nacimiento;
+    $tabla1->save();
+
+    $cliente_id = $tabla1->id;
+
+    $tabla2 = new Comentario();
+    $tabla2->cliente_id = $cliente_id;
+    $tabla2->descripcion = $descripcion;
+    $tabla2->expectativas = $expectativas;
+    $tabla2->horario = $horario;
+
+    $tabla2->save();
+    return redirect()->route('cita')->with('success', '¡El usuario se ha guardado exitosamente!');
+}
+
 
     public function show($id)
     {
--- frontLaravel/resources/views/administrador/registrarUsuario.blade.php
+++ frontLaravel/resources/views/administrador/registrarUsuario.blade.php
@@ -2,9 +2,52 @@
 
 @section('contentAdmin')
 <div>
-   
+  @if(session('success'))
+    <div class="alert alert-success">
 
-<section class=" py-1 bg-blueGray-50">
+        
+        <div class="bg-green-100 border-t-4 border-green-500 rounded-b px-4 py-3 shadow-md my-4">
+          <div class="flex items-center">
+              <div class="text-green-700">
+                  <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                  </svg>
+              </div>
+              <div class="mx-3">
+                  <span class="font-semibold text-green-700">Exito!</span>
+                  <p class="text-sm text-green-700">{{ session('success') }}</p>
+              </div>
+          </div>
+      </div>
+    </div>
+@endif
+  @if ($errors->any())
+  <div class="items-center">
+
+  <div class="bg-red-100 border-t-4 border-red-500 rounded-b px-4 py-3 shadow-md my-4 justify-center items-center">
+    <div class="flex items-center">
+        <div class="text-red-700">
+            <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </div>
+        <div class="mx-3">
+            <span class="font-semibold text-red-700">Error!</span>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </div>
+    </div>
+  </div>
+</div>
+@endif
+
+   
+  <form action="{{ route('guardar-usuario') }}" method="POST">
+    @csrf
+<section class="py-1 bg-blueGray-50">
+
+ 
 <div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
   <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
     <div class="rounded-t bg-white mb-0 px-6 py-6">
@@ -12,7 +55,7 @@
         <h6 class="text-blueGray-700 text-xl font-bold">
           Registra usuarios
         </h6>
-        <button class="bg-green-500 text-white active:bg-pink-800 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button">
+        <button type = "subtmit" class="bg-green-500 text-white active:bg-pink-800 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button">
           Agregar
         </button>
       </div>
@@ -28,7 +71,7 @@
               <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                 Nombre completo
               </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Mario bros">
+              <input name="nombre" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Mario bros">
             </div>
           </div>
           <div class="w-full lg:w-6/12 px-4">
@@ -36,7 +79,7 @@
               <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                 correo electronico
               </label>
-              <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="jesse@example.com">
+              <input name="email" type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="mario@example.com">
             </div>
           </div>
           <div class="w-full lg:w-6/12 px-4">
@@ -44,80 +87,46 @@
               <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                 Telefono
               </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="3356245869">
+              <input name="telefono" type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="3356245869">
             </div>
           </div>
           <div class="w-full lg:w-6/12 px-4">
             <div class="relative w-full mb-3">
               <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                Password
+                Role
               </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Jesse">
+              <select name="role" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Jesse">
+                <option>USER</option>
+                <option>ADMIN</option>
+              </select>
+            </div>
+          </div>
+          <div class="w-full lg:w-6/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                Contraseña
+              </label>
+              <input name="password" type="password" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
+            </div>
+          </div>
+          <div class="w-full lg:w-6/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
+                confirme su contraseña
+              </label>
+              <input name="password_confirmation" type="password" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" >
             </div>
           </div>
         </div>
-
         <hr class="mt-6 border-b-1 border-blueGray-300">
 
-        <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          Contact Information
-        </h6>
-        <div class="flex flex-wrap">
-          <div class="w-full lg:w-12/12 px-4">
-            <div class="relative w-full mb-3">
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                Address
-              </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
-            </div>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <div class="relative w-full mb-3">
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                City
-              </label>
-              <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="New York">
-            </div>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <div class="relative w-full mb-3">
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                Country
-              </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="United States">
-            </div>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <div class="relative w-full mb-3">
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                Postal Code
-              </label>
-              <input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Postal Code">
-            </div>
-          </div>
-        </div>
-
-        <hr class="mt-6 border-b-1 border-blueGray-300">
-
-        <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-          About Me
-        </h6>
-        <div class="flex flex-wrap">
-          <div class="w-full lg:w-12/12 px-4">
-            <div class="relative w-full mb-3">
-              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                About me
-              </label>
-              <textarea type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"> A beautiful UI Kit and Admin for JavaScript &amp; Tailwind CSS. It is Freeand Open Source.</textarea>
-            </div>
-          </div>
-        </div>
       </form>
     </div>
   </div>
   
 </div>
 </section>
-  
+</form> 
+
 </div>
 @endsection
