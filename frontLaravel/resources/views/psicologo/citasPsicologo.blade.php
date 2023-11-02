46fe5b58797b235e8ba5ef22247e16726fae7372
Nestor RV <nestor.regalado1453@alumnos.udg.mx>
2023-11-01 21:38:44 -0600

    Paso de parametros de usuario

--- frontLaravel/resources/views/psicologo/citasPsicologo.blade.php
+++ frontLaravel/resources/views/psicologo/citasPsicologo.blade.php
@@ -2,6 +2,9 @@
 
 @section('contentPsicologo')
 
-<div> Hola esta es la pgina de estadisticas</div>
+<div> Hola esta es la pgina de citas Psicologo</div>
+<td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $citasPsicologo->nombre }}</td>
+    <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $citasPsicologo->email }}</td>
+    <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ $citasPsicologo->telefono }}</td>
 
 @endsection
