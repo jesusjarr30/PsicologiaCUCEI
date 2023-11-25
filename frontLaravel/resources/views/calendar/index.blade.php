25a550e97414fad4c6428e1cbe2d9a0b348e9fb2
Nestor RV <nestor.regalado1453@alumnos.udg.mx>
2023-11-25 15:53:00 -0600

    Acomodos del calendario

--- frontLaravel/resources/views/calendar/index.blade.php
+++ frontLaravel/resources/views/calendar/index.blade.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +19,9 @@
 </head>
 <body>
 
+    @extends('layouts.BaseAdmin')
 
+    @section('contentAdmin')
   <!-- Modal -->
   <div class="modal fade" id="bookingModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -39,21 +42,48 @@
     </div>
   </div>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                <h3 class="text-center mt-5">FullCalendar js Laravel series with Career Development Lab</h3>
-                <div class="col-md-11 offset-1 mt-5 mb-5">
-
-                    <div id="calendar">
-
+            <div class="col- 12">
+                <h3 class="text-center mt-5">Gestionar citas</h3>
+                <section class="content">
+                    <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-md-2">
+                        <div class="sticky-top mb-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Pacientes pendientes</h4>
+                                </div>
+                                <div class="card-body">
+                                    <div id="external-events">
+                                        <div class="external-event bg-success ui-draggable ui-draggable-handle" style="position: relative; z-index: auto; left: 0px; top: 0px;">Suicidio</div>
+                                        <div class="external-event bg-warning ui-draggable ui-draggable-handle" style="position: relative;">Depresion</div>
+                                        <div class="external-event bg-info ui-draggable ui-draggable-handle" style="position: relative;">Ansiedad</div>
+                                        <div class="external-event bg-danger ui-draggable ui-draggable-handle" style="position: relative;">Otros</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    
+                    <div class="col-md-10">
+                        <div class="card card-primary">
+                            <div class="card-body p-0">
+                                <div id="calendar">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    </div>
+                    
+                    </div>
+                    </section>
+                
 
-                </div>
             </div>
         </div>
     </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script>
@@ -172,5 +202,7 @@
 
         });
     </script>
+    @endsection
+
 </body>
 </html>
