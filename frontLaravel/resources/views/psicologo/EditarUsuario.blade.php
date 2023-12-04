80a53fa2e20ab88c761cd77974f9be450a2bd9f5
Nestor RV <nestor.regalado1453@alumnos.udg.mx>
2023-12-04 03:39:10 -0600

    cambios menores a editar usuario

--- frontLaravel/resources/views/psicologo/EditarUsuario.blade.php
+++ frontLaravel/resources/views/psicologo/EditarUsuario.blade.php
@@ -167,6 +167,9 @@
                         @else
                           <option value="--:--">Inicio</option>
                         @endif
+                        <option value="00:00">Sin asignar</option>
+                        <option value="08:00">08:00 am</option>
+                        <option value="09:00">09:00 am</option>
                         <option value="10:00">10:00 am</option>
                         <option value="11:00">11:00 am</option>
                         <option value="12:00">12:00 pm</option>
@@ -176,8 +179,7 @@
                         <option value="16:00">04:00 pm</option>
                         <option value="17:00">05:00 pm</option>
                         <option value="18:00">06:00 pm</option>
-                        <option value="19:00">07:00 pm</option>
-                        <option value="20:00">08:00 pm</option>
+
 
                     </select>
                     <select
@@ -192,6 +194,9 @@
                         @else
                           <option value="--:--">Final</option>
                         @endif
+                        <option value="00:00">Sin asignar</option>
+                        <option value="08:00">08:00 am</option>
+                        <option value="09:00">09:00 am</option>
                         <option value="10:00">10:00 am</option>
                         <option value="11:00">11:00 am</option>
                         <option value="12:00">12:00 pm</option>
@@ -201,8 +206,7 @@
                         <option value="16:00">04:00 pm</option>
                         <option value="17:00">05:00 pm</option>
                         <option value="18:00">06:00 pm</option>
-                        <option value="19:00">07:00 pm</option>
-                        <option value="20:00">08:00 pm</option>
+
 
                     </select>
                   </div>
